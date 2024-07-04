e8844a12dbb9b82f3ad6916786cae3e56b767c34
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-07-04 23:12:11 +0100

    File that wraps a style around children elemets. It contains the header, which is displayed across all pages in the app

--- app/(content)/layout.js
+++ app/(content)/layout.js
@@ -1,6 +1,7 @@
 import MainHeader from '@/components/MainHeader';
 import '../globals.css';
 
+
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
